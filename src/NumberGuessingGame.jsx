47a1fbdd62d78383467c6d9b9f7f3e6715c2ba1c
mava1 <anamariamaldonado1994@gmail.com>
2025-01-20 12:17:35 -0500

    react-guessing-game-vite-Anamaria. React Router Class Components

--- src/NumberGuessingGame.jsx
+++ src/NumberGuessingGame.jsx
@@ -1,7 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import GuessControl from "./GuessControl";
 import GuessMessage from "./GuessMessage";
 import GameOver from "./GameOver";
+
+function NumberGuessingGame() {
+  const [numberToGuess, setNumberToGuess] = useState(getRandomNumber());
+  const [numberOfGuesses, setNumberOfGuesses] = useState(0);
+  const [latestGuess, setLatestGuess] = useState(null);
+
+  function handleGuess(guess) {
+    setLatestGuess(Number(guess));
+    setNumberOfGuesses(function (EdoPrevGuesses) {
+      return EdoPrevGuesses + 1;
+    });
+  }
+
+  function handleReset() {
+    setNumberToGuess(getRandomNumber());
+    setNumberOfGuesses(0);
+    setLatestGuess(null);
+  }
+
+  const isCorrectGuess = latestGuess === numberToGuess;
+
+  const isGameOver = isCorrectGuess || numberOfGuesses === MAX_ATTEMPTS;
+
+  return (
+    <div>
+      <h2>I'm thinking of a number from 1 to 100.</h2>
+      <h2>
+        Can you guess the number I am thinking of in {MAX_ATTEMPTS} tries?
+      </h2>
+      <GuessControl onGuess={handleGuess} />
+      {isGameOver && <GameOver hasWon={isCorrectGuess} onReset={handleReset} />}
+      {!isGameOver && (
+        <GuessMessage
+          guess={latestGuess}
+          numberToGuess={numberToGuess}
+          numberOfGuesses={numberOfGuesses}
+        />
+      )}
+    </div>
+  );
+}
 
 /**
  *
@@ -13,7 +54,7 @@ function getRandomNumber() {
 
 const MAX_ATTEMPTS = 5;
 
-class NumberGuessingGame extends Component {
+class NumberGuessingGameOld extends Component {
   constructor(props) {
     super(props);
 
